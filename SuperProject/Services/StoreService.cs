d67c54a0e2e2f761b50ff361b651b64e4b945041
gobs84 <gps9705@gmail.com>
2018-06-09 23:31:07 -0400

    FE-003: Changes in Read

--- SuperProject/Services/StoreService.cs
+++ SuperProject/Services/StoreService.cs
@@ -6,13 +6,12 @@ namespace SuperProject.Services
 {
     class StoreService
     {
-        List<Store> list = new List<Store>();
-
+        List<Store> dbStore = new List<Store>();
         public bool create(string name, string line1, string line2, int phone)
         {
             Store auxStore = new Store();
             auxStore.Name = name;
-            foreach (Store element in list)
+            foreach (Store element in dbStore)
             {
                 if (element.Name.Equals(name))
                 {
@@ -23,16 +22,27 @@ namespace SuperProject.Services
             auxStore.Line1 = line1;
             auxStore.Line2 = line2;
             auxStore.Phone = phone;
-            list.Add(auxStore);
+            dbStore.Add(auxStore);
             return true;
         }
         public List<Store> read()
         {
-            return list;
+            List<Store> dBstore = new List<Store>();
+            Store store1 = new Store() {Name="New York", Line1= "a", Line2="b",Phone= 1};
+            Store store2 = new Store() { Name = "Chicago", Line1 = "c", Line2 = "d", Phone = 2 };
+            Store store3 = new Store() { Name = "La Paz", Line1 = "e", Line2 = "f", Phone = 3 };
+            Store store4 = new Store() { Name = "Cochabamba", Line1 = "g", Line2 = "h", Phone = 4 };
+            Store store5 = new Store() { Name = "Moscu", Line1 = "i", Line2 = "j", Phone = 5 };
+            dBstore.Add(store1);
+            dBstore.Add(store2);
+            dBstore.Add(store3);
+            dBstore.Add(store4);
+            dBstore.Add(store5);
+            return dBstore;
         }
         public bool Update(string name, Store store)
         {
-            foreach (Store element in list)
+            foreach (Store element in dbStore)
             {
                 if (element.Name.Equals(name))
                 {
@@ -47,11 +57,11 @@ namespace SuperProject.Services
         }
         public bool Delete(string name)
         {
-            foreach (Store element in list)
+            foreach (Store element in dbStore)
             {
                 if (element.Name.Equals(name))
                 {
-                    list.Remove(element);
+                    dbStore.Remove(element);
                     return true;
                 }
             }
