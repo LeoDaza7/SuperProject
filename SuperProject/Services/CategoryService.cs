f03f8d4bdc1cfddaba1992a0a05de4ae9097eb0a
Leonardo Gonzales <Leocris26@hotmail.com>
2018-06-10 19:03:28 -0400

    FE-002: Added Model for the Category, and the CRUD operations on the Service

--- SuperProject/Services/CategoryService.cs
+++ SuperProject/Services/CategoryService.cs
@@ -6,20 +6,77 @@ namespace SuperProject.Services
 {
     class CategoryService
     {
-        public bool create()
+        public List<Category> categoryList;
+
+        public CategoryService()
         {
+            if (categoryList){
+                categoryList = new List<Category>();
+            }
+            
+        }
+
+        public bool create(string name, string description)
+        {
+            foreach (Category c in categoryList)
+            {
+                if (c.name == name)
+                {
+                    console.log("Name repeated or already exist!");
+                    return false;
+                }
+            }
+            Category nCategory = new Category();
+            nCategory.Name = name;
+            nCategory.Description = description;
+            categoryList.Add(nCategory);
             return true;
         }
+
         public List<Category> read()
         {
-            return null;
+            Category mock1 = new Category() {Name = "New", Description = "Not Used"};
+            Category mock2 = new Category() {Name = "Not so new", Description = "Slightly Used"};
+            Category mock3 = new Category() {Name = "Old", Description = "Used"};
+            Category mock4 = new Category() {Name = "Rounded", Description = "Without Corners"};
+            Category mock5 = new Category() {Name = "Squared", Description = "Without Curves"};
+            categoryList.Add(mock1);
+            categoryList.Add(mock2);
+            categoryList.Add(mock3);
+            categoryList.Add(mock4);
+            categoryList.Add(mock5);
+
+            return categoryList;
         }
-        public bool Update()
+
+        public bool Update(string key, Category uCategory)
         {
+            foreach (Category c in categoryList)
+            {
+                if (c.name == key)
+                {
+                    console.log("Updating");
+                    c.Name = uCategory.Name;
+                    c.Description = uCategory.Description;
+                    return true;
+                }
+            }
+            console.log("Ups, seems like that category doesn't exist");
             return false;
         }
-        public bool Delete()
+
+        public bool Delete(string key)
         {
+            foreach (Category c in categoryList)
+            {
+                if (c.name == key)
+                {
+                    console.log("Exterminating!!!");
+                    categoryList.Remove(c);
+                    return true;
+                }
+            }
+            console.log("Ups, seems like that category doesn't exist or is indestructible");
             return false;
         }
     }
