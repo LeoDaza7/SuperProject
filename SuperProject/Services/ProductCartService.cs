f6beb9bbaffd7fca1b36bb961dc189a008120bf1
chingolo7278 <37511558+chingolo7278@users.noreply.github.com>
2018-06-11 11:43:41 -0400

    Update ProductCartService.cs

--- SuperProject/Services/ProductCartService.cs
+++ SuperProject/Services/ProductCartService.cs
@@ -1,26 +1,74 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SuperProject.Services
 {
-    class ProductCartService
+   class ProductCartService
     {
-        public bool create()
+        public Cart cart;
+        public ProductCartService() { 
+        }
+        public ProductCartService(Cart cart)
         {
+            this.cart = cart;
+        }
+        private int getProductCartIndex(string productCode)
+        {
+            return cart.ListPC.FindIndex((x => x.ProductCode == productCode));
+        }
+
+        public bool productCartVerification(ProductCart pc)
+        {
+            return cart.ListPC.Exists((x => x.ProductCode == pc.ProductCode));
+        }
+
+
+        public bool Create(ProductCart pc)
+        {
+            if (productCartVerification(pc))
+            {
+                return false;
+            }
+
+            cart.ListPC.Add(pc);
             return true;
         }
-        public List<ProductCart> read()
+        public bool Delete(string key)
         {
-            return null;
+            int index = getProductCartIndex(key);
+            if (index != -1)
+            { 
+                cart.ListPC.RemoveAt(index);
+                return true;
+            }
+            return false;
+
+       
         }
-        public bool Update()
+        public List<ProductCart> getListPC()
         {
+            return cart.ListPC;
+        }
+
+        public bool Update(string key, ProductCart pc)
+        {
+            
+            if (key != pc.ProductCode || cart == null)
+            {
+                return false;
+            }
+            
+            int index = getProductCartIndex(key);
+            if (index != -1)
+            {
+                cart.ListPC[index] = pc;
+                return true;
+            }
             return false;
         }
-        public bool Delete()
-        {
-            return false;
-        }
+
     }
 }
