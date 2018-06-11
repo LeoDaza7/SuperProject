eebb9c0369d7b298aab61f448de1d308942a1d51
Leo D <37496762+LeoDaza7@users.noreply.github.com>
2018-06-11 00:45:20 -0400

    FE-005: User Model and Servie implemented.

--- SuperProject/Models/User.cs
+++ SuperProject/Models/User.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace SuperProject
 {
-    class User
+    class User : IEquatable<User>
     {
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public String Name { get; set; }
+        public String LastName { get; set; }
+        public List<ShippingAddress> ShippingAddresses { get; set; }
+        public User() { }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as User);
+        }
+
+        public bool Equals(User other)
+        {
+            return other != null &&
+                   Username == other.Username;
+        }
+
+        public override int GetHashCode()
+        {
+            return -182246463 + EqualityComparer<string>.Default.GetHashCode(Username);
+        }
     }
 }
