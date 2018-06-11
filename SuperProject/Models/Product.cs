f43254bb7963b106de85a2c9206a153e512e8e71
Nicolas Carrasco <chincholax@gmail.com>
2018-06-11 15:48:19 -0400

    FE-001: servicios CRUD implementados y libres de bugs

--- SuperProject/Models/Product.cs
+++ SuperProject/Models/Product.cs
@@ -7,15 +7,15 @@ namespace SuperProject
     class Product
     { 
 
-        private string code { get; set; }
-        private string name { get; set; }
-        private double price { get; set; }
-        private string description { get; set; }
-        private enum typeEnum : int { physical = 1, digital = 0, nulo = -1};
-        private typeEnum type { get; set; }
-        private enum shippingDeliveryTypeEnum : int { nulo = -1, express = 1, normal = 2, inStore = 3, free = 4, none = 0};
-        private shippingDeliveryTypeEnum shippingDeliveryType { get; set; }
-        private Category category { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public string description { get; set; }
+        public enum typeEnum : int { physical = 1, digital = 0, nulo = -1};
+        public typeEnum type { get; set; }
+        public enum shippingDeliveryTypeEnum : int { nulo = -1, express = 1, normal = 2, inStore = 3, free = 4, none = 0};
+        public shippingDeliveryTypeEnum shippingDeliveryType { get; set; }
+        public Category category { get; set; }
 
         public Product()
         {
@@ -28,5 +28,10 @@ namespace SuperProject
             category = null;
         }
 
+        public string toString()
+        {
+            return "Codigo = " + code + "\nNombre = " + name + "\nPrecio = " + price + "\nTipo = " + type + "\nTipo de entrega = " + shippingDeliveryType + "\nCategoria = " + category;
+        }
+            
     }
 }
--- SuperProject/Program.cs
+++ SuperProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SuperProject.Services;
+using System;
 
 namespace SuperProject
 {
@@ -6,7 +7,71 @@ namespace SuperProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProductService ps = new ProductService();
+            ps.ejemploLista();
+
+            Console.WriteLine("Lista de ejemplo para trabajar\n");
+            foreach(Product p in ps.read())
+            {
+                Console.WriteLine(p.toString());
+            }
+
+            Console.WriteLine("Introduciendo un codigo existente\n");
+            Product ej1 = new Product();
+            ej1.code = "1";
+            ej1.name = "Huawei Mate 10 Lite";
+            ej1.price = 270.75;
+            ej1.description = "RNE-L23";
+            ej1.type = Product.typeEnum.physical;
+            ej1.shippingDeliveryType = Product.shippingDeliveryTypeEnum.express;
+            ej1.category = null;
+            ps.create((Object) ej1);
+
+            Console.WriteLine("Introduciendo un codigo nuevo\n");
+            ej1.code = "31";
+            ps.create((Object) ej1);
+
+            Console.WriteLine("Revisando los cambios realizados\n");
+            foreach (Product p in ps.read())
+            {
+                Console.WriteLine(p.toString());
+            }
+
+            Console.WriteLine("Actualizando un producto inexistente\n");
+            Product ej2 = new Product();
+            ej2.code = "15";
+            ej2.name = "Huawei Mate 10 pro";
+            ej2.price = 270.75;
+            ej2.description = "...";
+            ej2.type = Product.typeEnum.physical;
+            ej2.shippingDeliveryType = Product.shippingDeliveryTypeEnum.express;
+            ej2.category = null;
+            ps.Update(ej2.code, (Object) ej2);
+
+            Console.WriteLine("Actualizando un producto existente\n");
+            ej2.code = "0";
+            ps.Update(ej2.code, (Object) ej2);
+
+            Console.WriteLine("Revisando los cambios realizados\n");
+            foreach (Product p in ps.read())
+            {
+                Console.WriteLine(p.toString());
+            }
+
+            Console.WriteLine("Borrando un producto que no existe en la lista la lista\n");
+            ps.Delete("5");
+
+            Console.WriteLine("Borrando un producto de la lista");
+            ps.Delete("1");
+
+            Console.WriteLine("Revisando los cambios realizados\n");
+            foreach (Product p in ps.read())
+            {
+                Console.WriteLine(p.toString());
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
