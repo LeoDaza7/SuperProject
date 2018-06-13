e24870647a4fb867dbcd892bb7e93acab5123388
marant <pensu15@gmail.com>
2018-06-13 00:01:26 -0400

    cleanups: Interface added

--- SuperProject/Models/Product.cs
+++ SuperProject/Models/Product.cs
@@ -7,30 +7,30 @@ namespace SuperProject
     class Product
     { 
 
-        public string code { get; set; }
-        public string name { get; set; }
-        public double price { get; set; }
-        public string description { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
         public enum typeEnum : int { physical = 1, digital = 0, nulo = -1};
-        public typeEnum type { get; set; }
+        public typeEnum Type { get; set; }
         public enum shippingDeliveryTypeEnum : int { nulo = -1, express = 1, normal = 2, inStore = 3, free = 4, none = 0};
-        public shippingDeliveryTypeEnum shippingDeliveryType { get; set; }
-        public Category category { get; set; }
+        public shippingDeliveryTypeEnum ShippingDeliveryType { get; set; }
+        public Category Category { get; set; }
 
         public Product()
         {
-            code = "";
-            name = "";
-            price = 0.0;
-            description = "";
-            type = typeEnum.nulo;
-            shippingDeliveryType = shippingDeliveryTypeEnum.nulo;
-            category = null;
+            Code = "";
+            Name = "";
+            Price = 0.0;
+            Description = "";
+            Type = typeEnum.nulo;
+            ShippingDeliveryType = shippingDeliveryTypeEnum.nulo;
+            Category = null;
         }
 
         public string toString()
         {
-            return "Codigo = " + code + "\nNombre = " + name + "\nPrecio = " + price + "\nTipo = " + type + "\nTipo de entrega = " + shippingDeliveryType + "\nCategoria = " + category;
+            return "Codigo = " + Code + "\nNombre = " + Name + "\nPrecio = " + Price + "\nTipo = " + Type + "\nTipo de entrega = " + ShippingDeliveryType + "\nCategoria = " + Category;
         }
             
     }
--- SuperProject/Services/ProductService.cs
+++ SuperProject/Services/ProductService.cs
@@ -1,113 +1,96 @@
-﻿using System;
+﻿using Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SuperProject.Services
 {
-    class ProductService
+    class ProductService : IService1<Product>
     {
         public List<Product> productos = new List<Product>();
+
+        private int GetIndex(string key)
+        {
+            return productos.FindIndex((x => x.Code == key));
+        }
+
+        private bool Verification(Product p)
+        {
+            return productos.Exists((x => x.Code == p.Code));
+        }
         public void ejemploLista()
         {
             Product p1 = new Product();
-            p1.code = "0";
-            p1.name = "Head Phones";
-            p1.price = 20.75;
-            p1.description = "Audifonos huawei";
-            p1.type = Product.typeEnum.physical;
-            p1.shippingDeliveryType = Product.shippingDeliveryTypeEnum.inStore;
-            p1.category = null;
+            p1.Code = "0";
+            p1.Name = "Head Phones";
+            p1.Price = 20.75;
+            p1.Description = "Audifonos huawei";
+            p1.Type = Product.typeEnum.physical;
+            p1.ShippingDeliveryType = Product.shippingDeliveryTypeEnum.inStore;
+            p1.Category = null;
             productos.Add(p1);
 
             Product p2 = new Product();
-            p2.code = "1";
-            p2.name = "Mouse";
-            p2.price = 17.5;
-            p2.description = "Mouse inalambrico para portatil";
-            p2.type = Product.typeEnum.physical;
-            p2.shippingDeliveryType = Product.shippingDeliveryTypeEnum.express;
-            p2.category = null;
+            p2.Code = "1";
+            p2.Name = "Mouse";
+            p2.Price = 17.5;
+            p2.Description = "Mouse inalambrico para portatil";
+            p2.Type = Product.typeEnum.physical;
+            p2.ShippingDeliveryType = Product.shippingDeliveryTypeEnum.express;
+            p2.Category = null;
             productos.Add(p2);
 
             Product p3 = new Product();
-            p3.code = "2";
-            p3.name = "Windows Pro";
-            p3.price = 120.75;
-            p3.description = "Licencia digital para activacion de Windows Pro";
-            p3.type = Product.typeEnum.digital;
-            p3.shippingDeliveryType = Product.shippingDeliveryTypeEnum.none;
-            p3.category = null;
+            p3.Code = "2";
+            p3.Name = "Windows Pro";
+            p3.Price = 120.75;
+            p3.Description = "Licencia digital para activacion de Windows Pro";
+            p3.Type = Product.typeEnum.digital;
+            p3.ShippingDeliveryType = Product.shippingDeliveryTypeEnum.none;
+            p3.Category = null;
             productos.Add(p3);
         }
 
-        public bool create(Object producto)
+        public bool Create(Product producto)
         {
-            bool estado = true;
-            Product aux = (Product) producto;
-            foreach(Product p in productos)
-            {
-                if (p.code.Equals(aux.code))
-                {
-                    Console.WriteLine("Intente con un nuevo codigo");
-                    estado = false;
-                }
-            }
+            bool estado = !Verification(producto);
             if (estado)
             {
-                productos.Add(aux);
+                productos.Add(producto);
             }
             return estado;
         }
 
-        public List<Product> read()
+        public List<Product> Read()
         {
             ejemploLista();
             return productos;
         }
-        public bool Update(string codigo, Object producto)
+
+        public bool Update(string codigo, Product producto)
         {
-            bool estado = false;
-            Product aux = (Product) producto;
-            foreach(Product p in productos)
+            bool estado = true;
+            if (!codigo.Equals(producto.Code))
+                estado = false;
+            if (estado)
             {
-                if(p.code.Equals(codigo))
-                {
-                    p.name = aux.name;
-                    p.price = aux.price;
-                    p.description = aux.description;
-                    p.type = aux.type;
-                    p.shippingDeliveryType = aux.shippingDeliveryType;
-                    estado = true;
-                    Console.WriteLine("Producto actualizado");
-                }
-            }
-            if(!estado)
-            {
-                Console.WriteLine("Producto inexistente");
+                int index = GetIndex(codigo);
+                if (index != -1)
+                    productos[index] = producto;
+                else
+                    estado = false;
             }
             return estado;
         }
+
         public bool Delete(String codigo)
         {
-            bool estado = false;
-            Product aux = new Product();
-            foreach(Product p in productos)
-            {
-                if (p.code.Equals(codigo))
-                {
-                    aux = p;
-                    estado = true;
-                }
-            }
-            if (!estado)
-            {
-                Console.WriteLine("Producto inexistente");
-            }
+            bool estado = true;
+            int index = GetIndex(codigo);
+            if (index != -1)
+                productos.RemoveAt(index);
             else
-            {
-                productos.Remove(aux);
-                Console.WriteLine("Producto eliminado");
-            }
+                estado = false;
             return estado;
         }
     }
