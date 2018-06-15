017a9236197a5264c27838604c02b68b28c177e7
gobs84 <gps9705@gmail.com>
2018-06-15 17:23:48 -0400

    API-002: Adding StoreController

---
+++ WAPI/Controllers/StoreController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAPI.Controllers
+{
+    public class StoreController: ApiController
+    {
+        [HttpGet]
+        [Route("api/getusers")]
+        public HttpResponseMessage GetStore()
+        {
+            StoreService storeservice = new StoreService();//Servicios
+            List<Store> store = storeservice.Read();
+            string productsJSON = JsonConvert.SerializeObject(store, Formatting.Indented);
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(productsJSON, Encoding.UTF8, "application/json");
+            return response;
+        }
+
+        [HttpPost, ActionName("postInfo")]
+        public HttpResponseMessage PostInfo(Object content)
+        {
+            try
+            {
+                var response = Request.CreateResponse(HttpStatusCode.OK);
+                response.Content = new StringContent(content.ToString(), Encoding.UTF8, "application/json");
+                return response;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something went wrong '{0}'",e);
+            }
+        }
+
+        /*[HttpPut, ActionName("updateInfo")]
+        public HttpResponseMessage UpdateInfo(Object res)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(res.ToString(), Encoding.UTF8, "application/json");
+            return response;
+        }
+
+        [HttpDelete, ActionName("DeleteInfo")]
+        public HttpResponseMessage DeleteInfo(Object id)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(id.ToString(), Encoding.UTF8, "application/json");
+            return response;
+        }*/
+    }
+}
+}
