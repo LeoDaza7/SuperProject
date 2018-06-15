f1284d068dcedfc5bd05de336195978c43c46a34
gobs84 <gps9705@gmail.com>
2018-06-15 19:11:16 -0400

    API-002: Fixing API methods

--- WAPI/Controllers/StoreController.cs
+++ WAPI/Controllers/StoreController.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SuperProject;
+using SuperProject.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Web;
+using System.Web.Http;
 
 namespace WAPI.Controllers
 {
     public class StoreController: ApiController
     {
         [HttpGet]
-        [Route("api/getusers")]
+        [Route("/api/getstore")]
         public HttpResponseMessage GetStore()
         {
             StoreService storeservice = new StoreService();//Servicios
@@ -24,13 +31,27 @@ namespace WAPI.Controllers
         {
             try
             {
-                var response = Request.CreateResponse(HttpStatusCode.OK);
-                response.Content = new StringContent(content.ToString(), Encoding.UTF8, "application/json");
+                String storeJSON = content.ToString();
+                Store store = JsonConvert.DeserializeObject<Store>(storeJSON);
+                StoreService ss = new StoreService();
+                var response = Request.CreateResponse(HttpStatusCode.Unused);
+                if (ss.Create(store))
+                {
+                    response = Request.CreateResponse(HttpStatusCode.OK);
+                    response.Content = new StringContent("Store created\n" + lista(ss.Read()), Encoding.UTF8, "application/json");
+                }
+                else
+                {
+                    response = Request.CreateResponse(HttpStatusCode.ExpectationFailed);
+                    response.Content = new StringContent("An error has ocurred creating the store", Encoding.UTF8, "application/json");
+                }
                 return response;
             }
             catch(Exception e)
             {
-                Console.WriteLine("Something went wrong '{0}'",e);
+                var response = Request.CreateResponse(HttpStatusCode.ExpectationFailed);
+                response.Content = new StringContent(("Error"+e),Encoding.UTF8, "application/json");
+                return response;
             }
         }
 
@@ -40,15 +61,35 @@ namespace WAPI.Controllers
             var response = Request.CreateResponse(HttpStatusCode.OK);
             response.Content = new StringContent(res.ToString(), Encoding.UTF8, "application/json");
             return response;
-        }
+        }*/
 
         [HttpDelete, ActionName("DeleteInfo")]
         public HttpResponseMessage DeleteInfo(Object id)
         {
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            response.Content = new StringContent(id.ToString(), Encoding.UTF8, "application/json");
-            return response;
-        }*/
+            try
+            {
+                String storeJSON = content.ToString();
+                string key = JsonConvert.DeserializeObject<String>(storeJSON);
+                storeService ss = new StoreService();
+                var response = Request.CreateResponse(HttpStatusCode.Unused);
+                if (ss.Delete(key))
+                {
+                    response = Request.CreateResponse(HttpStatusCode.OK);
+                    response.Content = new StringContent("Store deleted\n" + lista(ss.Read()), Encoding.UTF8, "application/json");
+                }
+                else
+                {
+                    response = Request.CreateResponse(HttpStatusCode.ExpectationFailed);
+                    response.Content = new StringContent("An error has ocurred deleting the store", Encoding.UTF8, "application/json");
+                }
+                return response;
+            }
+            catch (Exception e)
+            {
+                var response = Request.CreateResponse(HttpStatusCode.ExpectationFailed);
+                response.Content = new StringContent(("Error" + e), Encoding.UTF8, "application/json");
+                return response;
+            }
+        }
     }
-}
 }
