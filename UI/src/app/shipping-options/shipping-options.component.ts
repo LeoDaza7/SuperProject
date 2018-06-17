b543cb3c7dba8d06532af0ea17c9c79e6d95054c
Leo D <37496762+LeoDaza7@users.noreply.github.com>
2018-06-17 03:07:34 -0400

    UI-000: Basic components added.

---
+++ UI/src/app/shipping-options/shipping-options.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping-options',
+  templateUrl: './shipping-options.component.html',
+  styleUrls: ['./shipping-options.component.css']
+})
+export class ShippingOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
