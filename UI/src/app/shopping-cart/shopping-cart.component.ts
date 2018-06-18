58026a3268faa989a64741085e46f95809565128
Leonardo Gonzales <Leocris26@hotmail.com>
2018-06-18 01:10:04 -0400

    UI-004: Working on the Shopping Cart with mocks only from Frontend

--- UI/src/app/shopping-cart/shopping-cart.component.ts
+++ UI/src/app/shopping-cart/shopping-cart.component.ts
@@ -8,8 +8,40 @@ import { Component, OnInit } from '@angular/core';
 export class ShoppingCartComponent implements OnInit {
 
   constructor() { }
+  product: {
+    nombre : string;
+    price: number;
+    tipoDeEntrega: string[];
+    tiendaARecoger: string[];
+    cantidad: number;
+  };
+  carrito : any[] = [];
 
   ngOnInit() {
+    this.product = {
+      nombre : "Laptop",
+      price: 520,
+      tipoDeEntrega: ["auto", "bici"],
+      tiendaARecoger: ["central"],
+      cantidad: 1
+    };
+    this.carrito.push(this.product);
+    this.product = {
+      nombre : "Celular",
+      price: 320,
+      tipoDeEntrega: ["auto"],
+      tiendaARecoger: ["Sucursal 1"],
+      cantidad: 1
+    };
+    this.carrito.push(this.product);
+    this.product = {
+      nombre : "Reloj",
+      price: 420,
+      tipoDeEntrega: ["auto", "bici", "Correo"],
+      tiendaARecoger: ["central", "Sucursal 2"],
+      cantidad: 1
+    };
+    this.carrito.push(this.product);
   }
 
 }
