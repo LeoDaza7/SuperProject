2262154ad897d2fba461af870380dbb94d2c6b7d
Leonardo Gonzales Vargas <leonardo.gonzales@fundacion-jala.org>
2018-06-19 11:43:15 -0400

    UI-004: Made the page works with the service

--- UI/src/app/shopping-cart/shopping-cart.component.ts
+++ UI/src/app/shopping-cart/shopping-cart.component.ts
@@ -3,6 +3,7 @@ import { Cart } from '../models/cart';
 import { ProductCart } from '../models/productCart';
 import { Store } from '../models/store';
 import { Product } from '../models/product';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -11,32 +12,43 @@ import { Product } from '../models/product';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private allService: HttpService) { }
   
-  product : Product;
-  items : ProductCart[] = [];
+  products : Product[];
   carrito : Cart;
   store: Store;
+  totalItems: number = 0;
+  totalPrice: number = 0;
   ngOnInit() {
-    this.store = new Store();
-    this.store.name = "Oxxo";
-    this.store.lane1 = "Lane1";
-    this.store.lane2 = "LaneUno";
-    this.store.phone = 79763702;
 
-    this.items.push({productCode : "ABCssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss", shippingDeliveryType: 2, store : this.store, quantity : 5})
-    this.items.push({productCode : "123", shippingDeliveryType: 4, store : this.store, quantity : 2})
-    this.items.push({productCode : "JQK", shippingDeliveryType: 1, store : this.store, quantity : 1})
-    this.items.push({productCode : "456", shippingDeliveryType: 3, store : this.store, quantity : 7})
-    this.items.push({productCode : "456", shippingDeliveryType: 3, store : this.store, quantity : 7})
-    this.items.push({productCode : "456", shippingDeliveryType: 3, store : this.store, quantity : 7})
-    this.items.push({productCode : "456", shippingDeliveryType: 3, store : this.store, quantity : 7})
+    this.getCart();
 
-    this.items.forEach(i => {
-      console.log("Call the service");
-    });
-    console.log(this.items);
-    
   }
 
+  getCart(){
+    this.allService.getObject("api/getCart", "User").subscribe(
+      response => {
+        this.carrito = response;
+        this.getProducts();
+      },
+      error => {
+        console.log(error);
+      }
+
+    );
+  }
+  getProducts(){
+    this.carrito.listPC.forEach(pc => {
+      let tmp = this.allService.getObject("api/getproducts",pc.productCode).subscribe(
+        response => {
+          this.products.push(tmp);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+      
+    });
+
+  }
 }
