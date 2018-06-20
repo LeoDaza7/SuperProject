67a5d8b01d6b0938426051a31f666319e6d49e3e
Leo D <37496762+LeoDaza7@users.noreply.github.com>
2018-06-19 20:49:26 -0400

    UI-005: Some Fixes.

--- UI/src/app/shipping-options/shipping-options.component.ts
+++ UI/src/app/shipping-options/shipping-options.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../http.service';
+//import { HttpService } from '../http.service';
 import { NgForm } from '@angular/forms';
 import {ShippingOptions} from './shipping-options.model';
 import {User} from '../user.model';
@@ -7,11 +7,11 @@ import {User} from '../user.model';
   selector: 'app-shipping-options',
   templateUrl: './shipping-options.component.html',
   styleUrls: ['./shipping-options.component.css'],
-  providers:[ShippingOptionsService]
+  providers:[ShippingOptions]
 })
 export class ShippingOptionsComponent implements OnInit {
 
-  constructor(private allService: HttpService) { }
+  constructor(private allService){ }
 
   shippingAddresses : ShippingOptions[];
   user : User;
@@ -24,12 +24,12 @@ export class ShippingOptionsComponent implements OnInit {
    if (form != null)
       form.reset();
     this.allService = {
-    Identifier : null;
-    Line1 : '';
-    Line2 : '';
-    Phone : '';
-    City : '';
-    Zone : '';
+    Identifier : null,
+    Line1 : '',
+    Line2 : '',
+    Phone : '',
+    City : '',
+    Zone : '',
     }
   }
   
@@ -38,7 +38,7 @@ export class ShippingOptionsComponent implements OnInit {
     this.user.ShippingAddresses.forEach(result => {
       this.allService.getObject("api/getshippingoptions",result.Identifier).subscribe(
         response => {
-          this.ShippingAddresses.push(response);
+          this.shippingAddresses.push(response);
         },
         error => {
           console.log(error);
@@ -50,8 +50,8 @@ export class ShippingOptionsComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    if (form.value.Identifier == null) {
-      this.allService.postObject("api/postshippingoptions",Codeform.value)
+    if (form.value.Identifier != null) {
+      this.allService.postObject("postshippingoptions",form.value.Identifier)
         .subscribe(data => {
           this.resetForm(form);
           
