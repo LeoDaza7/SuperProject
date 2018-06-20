344cd89da30ee66e355dcd0de7906d081ebea156
Nicolas Carrasco <chincholax@gmail.com>
2018-06-20 19:43:39 -0400

    UI: bug fixes product detail

--- UI/src/app/product-detail/product-detail.component.ts
+++ UI/src/app/product-detail/product-detail.component.ts
@@ -2,27 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { Cart } from '../models/cart';
 import { Product } from '../models/product';
 import { HttpService } from '../http.service';
+
+import { Router } from '@angular/router';
 import { ProductCart } from '../models/productCart';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
+  template: 'The href is: {{href}}'
 })
+
+
+  
+
+ 
 export class ProductDetailComponent implements OnInit {
 
-  constructor(private allService: HttpService) { }
-
+  constructor(private allService: HttpService ,private router: Router ) { }
+ 
   products : Product[];
   product : Product;
   productCart: ProductCart;
   carrito : Cart;
   id : String;
+  href : String;
+  cad : String[];
 
   ngOnInit() {
     this.getCart();
-    this.getProductCart();
     this.getProducts();
+   
+    this.href = this.router.url;
+    this.getId();
     this.getSpecific();
   }
 
@@ -68,12 +80,15 @@ export class ProductDetailComponent implements OnInit {
     );
   }
   getSpecific(){
-   this.product = this.products.find(i => i.Code === this.id);
+   this.product = this.products.find(i => i.code === this.id);
 
   }
   onClick(){
     this.carrito.listPC.push(this.productCart);
-
+  }
+  getId(){
+this.cad = this.href.split("/");
+this.id = this.cad[this.cad.length-1]; 
   }
 
 }
