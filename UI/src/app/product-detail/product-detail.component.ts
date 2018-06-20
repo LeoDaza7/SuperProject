c43acf8c2b8d26e51c303d6199e60c957c3fe148
chingolo7278 <37511558+chingolo7278@users.noreply.github.com>
2018-06-20 09:39:47 -0400

    UI-003: Page Three

--- UI/src/app/product-detail/product-detail.component.ts
+++ UI/src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from '../models/cart';
+import { Product } from '../models/product';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private allService: HttpService) { }
+
+  products : Product[];
+  product : Product;
+ // carrito : Cart;
+  id : String;
 
   ngOnInit() {
+   // this.getCart();
+    this.getProducts();
+    this.getSpecific();
   }
+/*
+  getCart(){
+    this.allService.getObject("getCart", "user1").subscribe(
+      response => {
+        console.log(response);
+        this.carrito = response;
+       
+          this.getProducts();
+      },
+      error => {
+        console.log(error);
+      }
 
+    );
+  }*/
+  getProducts(){
+
+    this.allService.getObject("getproducts","").subscribe(
+      response => {
+        console.log(response);
+        this.products = response;
+      },
+      error => {
+        console.log(error);
+      }
+
+    );
+  }
+  getSpecific(){
+   this.product = this.products.find(i => i.code === this.id);
+
+  }
 }
