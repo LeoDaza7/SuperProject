d0eb8939e06a73838a4f38ba00f291869330cbff
heidychoque <kete_heidy@hotmail.com>
2018-06-20 07:19:46 -0400

    UI-005: ShippingAdress Updated

--- UI/src/app/shipping-options/shipping-options.component.ts
+++ UI/src/app/shipping-options/shipping-options.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { ShippingAddress } from '../models/shipping-address';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shipping-options',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShippingOptionsComponent implements OnInit {
 
-  constructor() { }
+ getdata = {}
+  constructor(private service: HttpService) { }
+
+  shippingAddresses : ShippingAddress[];
+  
 
   ngOnInit() {
+  this.getShippingAddresses();
+  }
+
+  resetForm(form?: NgForm) {
+   if (form != null)
+      form.reset();
+    this.getdata = {
+    Identifier : null,
+    Line1 : '',
+    Line2 : '',
+    Phone : '',
+    City : '',
+    Zone : '',
+    }
+  }
+
+  getShippingAddresses(){
+    this.service.getObject("getshippingAddresses",null).subscribe(
+      res=>{
+        console.log(res)
+        this.shippingAddresses = res;
+      },
+      err=>{
+        console.log(err)
+      });
+  }
+
+  postShippingAddresses() {
+    this.service.postObject(this.getdata,"postshippingAddresses")
+    .subscribe(
+      res => console.log(res),
+      err => console.log(err)
+    )
   }
 
 }
