35c6d8f9465b605cada888c7e0657fe8671eccb8
gobs84 <gps9705@gmail.com>
2018-06-25 21:00:03 -0400

    UI-008: Adding cookies for log-in

--- UI/src/app/nav-bar/nav-bar.component.ts
+++ UI/src/app/nav-bar/nav-bar.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { DataSharingService } from '../data-sharing.service';
 
+import { CookieService } from 'ngx-cookie-service';
+import { User } from '../models/user';
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  cookieValue = 'UNKNOWN';
+  user:User = new User();
   show:boolean = true;
 
-  constructor(private _shared: DataSharingService) { }
+  constructor(private _shared: DataSharingService, private cookieService: CookieService) {
+    this.user.Username="";
+   }
 
   ngOnInit() {
     //this.show = this._shared.getStatus();
+    this.user.Username = this.cookieService.get('User')
   }
 
 }
